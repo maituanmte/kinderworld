a6cdd0164adf28688b1c191fe53274a872d674a4
Mai Quang Tuan <maituanmte@gmail.com>
2014-04-16 15:03:09 +0700

    Initial commit

---
+++ unit/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Year(models.Model):
+    pass
+
+class Unit(models.Model):
+    pass
+
+class Grade(models.Model):
+    pass
+
+class Class(models.Model):
+    pass
